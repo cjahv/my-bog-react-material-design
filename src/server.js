8aa91bdf4e8143336eee40a9fb4fe2fa693ad570
jahv <cjahv@qq.com>
2016-10-25 22:01:29 +0800

    Remove unnecessary code

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ import UniversalRouter from 'universal-router';
 import PrettyError from 'pretty-error';
 import App from './components/App';
 import Html from './components/Html';
-import { ErrorPageWithoutStyle } from './routes/error/ErrorPage';
+import {ErrorPageWithoutStyle} from './routes/error/ErrorPage';
 import errorPageStyle from './routes/error/ErrorPage.css';
 import passport from './core/passport';
 import models from './data/models';
@@ -30,14 +30,14 @@ import routes from './routes';
 import assets from './assets'; // eslint-disable-line import/no-unresolved
 import https from 'https';
 import fs from "fs";
-import { port, auth,https_port } from './config';
+import {port, auth, https_port} from './config';
 
 const app = express();
 
 const options = {
-  key: fs.readFileSync('./build/service.key'),
-  cert: fs.readFileSync('./build/service.crt'),
-  passphrase:'jahv'
+    key: fs.readFileSync('./build/service.key'),
+    cert: fs.readFileSync('./build/service.crt'),
+    passphrase: 'jahv'
 };
 
 //
@@ -52,82 +52,82 @@ global.navigator.userAgent = global.navigator.userAgent || 'all';
 // -----------------------------------------------------------------------------
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //
 // Authentication
 // -----------------------------------------------------------------------------
 app.use(expressJwt({
-  secret: auth.jwt.secret,
-  credentialsRequired: false,
-  getToken: req => req.cookies.id_token,
+    secret: auth.jwt.secret,
+    credentialsRequired: false,
+    getToken: req => req.cookies.id_token,
 }));
 app.use(passport.initialize());
 
 app.get('/login/facebook',
-  passport.authenticate('facebook', { scope: ['email', 'user_location'], session: false })
+    passport.authenticate('facebook', {scope: ['email', 'user_location'], session: false})
 );
 app.get('/login/facebook/return',
-  passport.authenticate('facebook', { failureRedirect: '/login', session: false }),
-  (req, res) => {
-    const expiresIn = 60 * 60 * 24 * 180; // 180 days
-    const token = jwt.sign(req.user, auth.jwt.secret, { expiresIn });
-    res.cookie('id_token', token, { maxAge: 1000 * expiresIn, httpOnly: true });
-    res.redirect('/');
-  }
+    passport.authenticate('facebook', {failureRedirect: '/login', session: false}),
+    (req, res) => {
+        const expiresIn = 60 * 60 * 24 * 180; // 180 days
+        const token = jwt.sign(req.user, auth.jwt.secret, {expiresIn});
+        res.cookie('id_token', token, {maxAge: 1000 * expiresIn, httpOnly: true});
+        res.redirect('/');
+    }
 );
 
 //
 // Register API middleware
 // -----------------------------------------------------------------------------
 app.use('/graphql', expressGraphQL(req => ({
-  schema,
-  graphiql: true,
-  rootValue: { request: req },
-  pretty: process.env.NODE_ENV !== 'production',
+    schema,
+    graphiql: true,
+    rootValue: {request: req},
+    pretty: process.env.NODE_ENV !== 'production',
 })));
 
 //
 // Register server-side rendering middleware
 // -----------------------------------------------------------------------------
-app.get('*', async (req, res, next) => {
-  try {
-    const css = new Set();
+app.get('*', async(req, res, next) => {
+    try {
+        const css = new Set();
 
-    // Global (context) variables that can be easily accessed from any React component
-    // https://facebook.github.io/react/docs/context.html
-    const context = {
-      // Enables critical path CSS rendering
-      // https://github.com/kriasoft/isomorphic-style-loader
-      insertCss: (...styles) => {
-        // eslint-disable-next-line no-underscore-dangle
-        styles.forEach(style => css.add(style._getCss()));
-      },
-    };
+        // Global (context) variables that can be easily accessed from any React component
+        // https://facebook.github.io/react/docs/context.html
+        const context = {
+            // Enables critical path CSS rendering
+            // https://github.com/kriasoft/isomorphic-style-loader
+            insertCss: (...styles) => {
+                // eslint-disable-next-line no-underscore-dangle
+                styles.forEach(style => css.add(style._getCss()));
+            },
+        };
 
-    const route = await UniversalRouter.resolve(routes, {
-      path: req.path,
-      query: req.query,
-    });
+        const route = await UniversalRouter.resolve(routes, {
+            path: req.path,
+            query: req.query,
+        });
 
-    if (route.redirect) {
-      res.redirect(route.status || 302, route.redirect);
-      return;
+        if (route.redirect) {
+            res.redirect(route.status || 302, route.redirect);
+            return;
+        }
+
+        const data = {...route};
+        data.children = ReactDOM.renderToString(<App context={context}>{route.component}</App>);
+        data.style = [...css].join('');
+        data.script = assets.main.js;
+        data.chunk = assets[route.chunk] && assets[route.chunk].js;
+        const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
+
+        res.status(route.status || 200);
+        res.send(`<!DOCTYPE html>${html}`);
+    } catch (err) {
+        next(err);
     }
-
-    const data = { ...route };
-    data.children = ReactDOM.renderToString(<App context={context}>{route.component}</App>);
-    data.style = [...css].join('');
-    data.script = assets.main.js;
-    data.chunk = assets[route.chunk] && assets[route.chunk].js;
-    const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
-
-    res.status(route.status || 200);
-    res.send(`<!doctype html>${html}`);
-  } catch (err) {
-    next(err);
-  }
 });
 
 //
@@ -138,18 +138,18 @@ pe.skipNodeFiles();
 pe.skipPackage('express');
 
 app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  console.log(pe.render(err)); // eslint-disable-line no-console
-  const html = ReactDOM.renderToStaticMarkup(
-    <Html
-      title="Internal Server Error"
-      description={err.message}
-      style={errorPageStyle._getCss()} // eslint-disable-line no-underscore-dangle
-    >
-      {ReactDOM.renderToString(<ErrorPageWithoutStyle error={err} />)}
-    </Html>
-  );
-  res.status(err.status || 500);
-  res.send(`<!doctype html>${html}`);
+    console.log(pe.render(err)); // eslint-disable-line no-console
+    const html = ReactDOM.renderToStaticMarkup(
+        <Html
+            title=":( Internal Server Error"
+            description={err.message}
+            style={errorPageStyle._getCss()} // eslint-disable-line no-underscore-dangle
+        >
+        {ReactDOM.renderToString(<ErrorPageWithoutStyle error={err}/>)}
+        </Html>
+    );
+    res.status(err.status || 500);
+    res.send(`<!DOCTYPE html>${html}`);
 });
 
 //
@@ -157,12 +157,12 @@ app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 // -----------------------------------------------------------------------------
 /* eslint-disable no-console */
 models.sync().catch(err => console.error(err.stack)).then(() => {
-  app.listen(port, () => {
-    console.log(`The server is running at http://localhost:${port}/`);
-  });
+    app.listen(port, () => {
+        console.log(`The server is running at http://localhost:${port}/`);
+    });
 
-  https.createServer(options, app).listen(https_port, function () {
-    console.log('Https server listening on port ' + https_port);
-  });
+    https.createServer(options, app).listen(https_port, () => {
+        console.log('Https server listening on port ' + https_port);
+    });
 });
 /* eslint-enable no-console */
